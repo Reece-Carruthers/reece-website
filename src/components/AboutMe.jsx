af8c1578a1d2079c6f4ea20c8528490dae447635
Reece Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-01-22 15:49:51 +0000

    Update about me and university experiences

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,8 +4,7 @@ export default function AboutMe() {
     return (
         <div className="flex flex-col rounded-xl border p-2 lg:p-5">
             <h2 className="text-xl lg:text-2xl 2xl:text-4xl m-2">About me</h2>
-            <p className={paragraphStyle}>I am in my final year of university studying Computer Science BSc Hons at Northumbria University</p>
-            <p className={paragraphStyle}>I am also working part-time as a Scala software engineer at HM Revenue & Customs</p>
+            <p className={paragraphStyle}>I am currently in my final year of University, studying Computer Science at Northumbria University, while I am working part-time as a Scala software engineer at HM Revenue & Customs. I would describe myself as a person with a strong work ethic who takes pride in their work and has strong critical thinking skills, which helps me to solve problems. I would also consider myself to be a happy and approachable individual who is easy-going, but serious when the time calls for it.</p>
             <p className={paragraphStyle}>I have a particular interest in Java, Scala and React</p>
         </div>
     )
--- src/components/experience/experienceType/UniversityExperience.jsx
+++ src/components/experience/experienceType/UniversityExperience.jsx
@@ -1,7 +1,7 @@
 export default function UniversityExperience() {
     return (
         <>
-            <p className="ml-2 mr-2 text-sm lg:text-base 2xl:text-2xl">During my degree at Northumbria University I have gained lots of valuable skills</p>
+            <p className="ml-2 mr-2 text-sm lg:text-base 2xl:text-2xl">During my degree at Northumbria University, I have had the opportunity to develop my personal and professional skills</p>
             <div className="mt-2 grid grid-cols-1">
                 <h3 className="text-lg lg:text-xl 2xl:text-2xl m-2 text-center">Skills Gained</h3>
                 <ul className="grid md:grid-cols-2 list-disc justify-center w-3/4 mx-auto text-sm lg:text-base 2xl:text-xl">
@@ -16,6 +16,9 @@ export default function UniversityExperience() {
                     <li>Design Patterns</li>
                     <li>User Research</li>
                     <li>Communication</li>
+                    <li>Backend</li>
+                    <li>Frontend</li>
+                    <li>Restful APIs</li>
                 </ul>
             </div>
         </>
