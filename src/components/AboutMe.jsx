547f84aaa199b61d845f67650d51b31edd0733ad
Reece Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-01-22 16:29:53 +0000

    Add additional information and styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,11 @@
 export default function AboutMe() {
 
-    const paragraphStyle = "text-sm lg:text-base 2xl:text-2xl ml-2 mr-2 m-2"
+    const paragraphStyle = "text-sm lg:text-base 2xl:text-xl ml-2 mr-2 m-2 text-justify text-wrap"
     return (
-        <div className="flex flex-col rounded-xl border p-2 lg:p-5">
+        <div className="flex flex-col rounded-xl border p-2 lg:p-5 lg:w-2/3 mx-auto justify-center items-center">
             <h2 className="text-xl lg:text-2xl 2xl:text-4xl m-2">About me</h2>
-            <p className={paragraphStyle}>I am currently in my final year of University, studying Computer Science at Northumbria University, while I am working part-time as a Scala software engineer at HM Revenue & Customs. I would describe myself as a person with a strong work ethic who takes pride in their work and has strong critical thinking skills, which helps me to solve problems. I would also consider myself to be a happy and approachable individual who is easy-going, but serious when the time calls for it.</p>
+            <p className={paragraphStyle}>I am currently in my final year of University, studying Computer Science at Northumbria University, while I am working part-time as a Scala software engineer at HM Revenue & Customs.</p>
+            <p className={paragraphStyle}> I would describe myself as a person with a strong work ethic who takes pride in their work and has strong critical thinking skills, which helps me to solve problems. I would also consider myself to be a happy and approachable individual who is easy-going, but serious when the time calls for it.</p>
             <p className={paragraphStyle}>I have a particular interest in Java, Scala and React</p>
         </div>
     )
--- src/components/experience/experienceType/UniversityExperience.jsx
+++ src/components/experience/experienceType/UniversityExperience.jsx
@@ -1,24 +1,31 @@
 export default function UniversityExperience() {
+    const skills = ["Java",
+        "React",
+        "C",
+        "PHP",
+        "Python",
+        "SQLite",
+        "HTML",
+        "CSS",
+        "Tailwind",
+        "Design Patterns",
+        "User Research",
+        "Communication",
+        "Backend",
+        "Frontend",
+        "Restful APIs",
+        "Problem Solving"
+    ]
+
     return (
         <>
-            <p className="ml-2 mr-2 text-sm lg:text-base 2xl:text-2xl">During my degree at Northumbria University, I have had the opportunity to develop my personal and professional skills</p>
+            <p className="mt-6 ml-2 mr-2 text-sm lg:text-base 2xl:text-2xl">During my degree at Northumbria University, I have had the opportunity to develop my personal and professional skills</p>
             <div className="mt-2 grid grid-cols-1">
                 <h3 className="text-lg lg:text-xl 2xl:text-2xl m-2 text-center">Skills Gained</h3>
                 <ul className="grid md:grid-cols-2 list-disc justify-center w-3/4 mx-auto text-sm lg:text-base 2xl:text-xl">
-                    <li>Java</li>
-                    <li>React</li>
-                    <li>C</li>
-                    <li>PHP</li>
-                    <li>SQLite</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Tailwind</li>
-                    <li>Design Patterns</li>
-                    <li>User Research</li>
-                    <li>Communication</li>
-                    <li>Backend</li>
-                    <li>Frontend</li>
-                    <li>Restful APIs</li>
+                    {skills.map((skill, index) => (
+                        <li className="mt-2" key={index}>{skill}</li>
+                    ))}
                 </ul>
             </div>
         </>
