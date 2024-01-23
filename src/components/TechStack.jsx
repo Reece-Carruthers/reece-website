60303403e6e5df76c1178fa1fc619753903b937f
Reece Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-01-23 19:16:25 +0000

    Add a tech stack with icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,6 +1,80 @@
 export default function TechStack() {
-
     return (
-        <p>TODO</p>
+        <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scala/scala-original-wordmark.svg"
+                        alt="Scala"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
+                        alt="Java"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                        alt="React"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+                        alt="Python"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                        alt="JavaScript"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+                        alt="PHP"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"
+                        alt="Django"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
+                        alt="Tailwind CSS"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+                        alt="MongoDB"
+                        width={158}
+                        height={48}
+                    />
+                    <img
+                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg"
+                        alt="SQLite"
+                        width={158}
+                        height={48}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
