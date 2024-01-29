0ea7387cc77bcd1c1d6119de5a510121cbb745e0
Reece Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-01-29 16:54:07 +0000

    Update experience section with accordions

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,8 +2,7 @@ import ExperienceType from "./ExperienceType.jsx";
 
 export default function Experience() {
 
-    const hmrcExperience = {
-        description: "I have worked at HM Revenue & Customs since the 5th of September 2022 until present",
+    const hmrcAccordionItems = {
         skills: [
             "Time Management",
             "Working under pressure",
@@ -21,11 +20,16 @@ export default function Experience() {
             "Scrum",
             "MongoDB",
             "Requirements Analysis"
+        ],
+        generalAccordion: [
+            {
+                accordionTitle: "Main Duties",
+                accordionText: "I worked on the New Computerised Transit System Phase 5 <To Be Completed>",
+            }
         ]
     }
 
-    const universityExperience = {
-        description: "During my degree at Northumbria University, I have had the opportunity to develop my personal and professional skills",
+    const universityAccordionItems = {
         skills: [
             "React",
             "C",
@@ -42,8 +46,28 @@ export default function Experience() {
             "Frontend",
             "Restful APIs",
             "Problem Solving"
+        ],
+        generalAccordion: [
+            {
+                accordionTitle: "Main Duties",
+                accordionText: "<To Be Completed>",
+            }
         ]
     }
+
+    const hmrcExperience = {
+        title: "HM Revenue and Customs (Sept 2022 - Present)",
+        description: "I have been working at HM Revenue and Customs for the past two years as a Scala software engineer",
+        accordionItems: hmrcAccordionItems
+    }
+
+    const universityExperience = {
+        title: "Northumbria University (Sept 2018 - Present)",
+        description: "During my degree at Northumbria University, I have had the opportunity to develop my personal and professional skills",
+        accordionItems: universityAccordionItems
+    }
+
+    const experiences = [hmrcExperience, universityExperience]
 
 
     return (
@@ -52,9 +76,16 @@ export default function Experience() {
             <div className="px-4 py-5 sm:px-6">
                 <h3 className="text-gray-900 text-xl font-bold">Relevant Experience</h3>
             </div>
-            <div className="px-4 py-5 sm:p-6 text-justify">
-                <ExperienceType description={hmrcExperience.description} skills={hmrcExperience.skills}/>
-                <ExperienceType description={universityExperience.description} skills={universityExperience.skills}/>
+            <div className=" mb-5 md:mb-10">
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <ul role="list" className="space-y-3">
+                        {experiences.map((experience, index) => (
+                            <li key={index} className="overflow-hidden bg-white px-4 py-4 shadow sm:rounded-md sm:px-6 mb-8">
+                                <ExperienceType title={experience.title} description={experience.description} accordionItems={experience.accordionItems} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
